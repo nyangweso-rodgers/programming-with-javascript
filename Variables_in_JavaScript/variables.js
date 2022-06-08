ab2836a72e7c38f1c0721c6c48b0b3f7fd73131f
Rodgers Nyangweso <rodgerso65@gmail.com>
2022-06-08 15:18:37 +0300

    modified the Variables folder

---
+++ Variables_in_JavaScript/variables.js
@@ -0,0 +1,8 @@
+// Method 1: using let keyword
+let message;
+
+// now, we can put some data into it by using the assignment operator =;
+message = 'Hallo World!'; // store the string
+
+// the string is now saved into the memory area associated with the variable and we can access it using the variable name
+console.log(message); // Output: Hallo World!
