229f32f5a6363b27ae64f47ec550d758a3000e15
Rodgers Nyangweso <rodgerso65@gmail.com>
2022-06-09 22:49:01 +0300

    first script for random number generation

---
+++ Object_Oriented_Programming_in_JavaScript/Array_Objects/script_1_random_number_generation.js
@@ -0,0 +1,7 @@
+// Generate an arry of numbers with numbers form 0 to max
+var numbersArray = [], max = 100;
+
+for (
+    var i=1; numbersArray.push(i++) < max;
+);
+console.log(numbersArray); // Output: numbers = [1, 2, 3, -- 100]
