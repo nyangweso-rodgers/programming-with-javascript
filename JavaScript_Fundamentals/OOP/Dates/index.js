5eafab63bc0bbfe9fca5e6fded8586abf8bfbdcb
Rodgers Nyangweso <rodgerso65@gmail.com>
2022-11-30 21:30:59 +0300

    ++ Dates Objects

---
+++ JavaScript_Fundamentals/OOP/Dates/index.js
@@ -0,0 +1,15 @@
+
+const timeNow = new Date();
+console.log(timeNow); // Output: 2022-11-30T18:15:12.393Z
+
+let date = timeNow.getDate();
+console.log(date); // Output: 30
+
+let day = timeNow.getDay();
+console.log(day); // Output: 3
+
+let year = timeNow.getFullYear();
+console.log(year); // Output: 2022
+
+let hour = timeNow.getHours();
+console.log(hour); // Output: 2022
