b457fac139bb2966bc2d45d7b5a9f62a91f2ef4d
Rodgers Nyangweso <rodgerso65@gmail.com>
2022-12-06 21:51:00 +0300

    ++ while loops

---
+++ JavaScript_Fundamentals/Control_Flow/while_loop/index.js
@@ -0,0 +1,13 @@
+// while loops
+
+var c = 1;
+
+while (c<=10) {
+    console.log(c + "<br/>");
+    c++;
+
+    // the loop will continue to run as long as c is less than, or equal 10. each time the loop runs, it increases by 1
+    // be careful writing conditions, if a condition is always true, the loop will run forever
+    // if you fail to increase the variable used in the condition, the loop willl neer end
+    // make sure that the condition in a while loop eventually becomes false
+};
