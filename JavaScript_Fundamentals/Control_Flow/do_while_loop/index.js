b9cf9bf7611afc50defcabb4b274c4ea17919740
Rodgers Nyangweso <rodgerso65@gmail.com>
2022-12-06 21:54:54 +0300

    ++ do while loops contents

---
+++ JavaScript_Fundamentals/Control_Flow/do_while_loop/index.js
@@ -0,0 +1,10 @@
+// do -- while loop
+
+var d = 20;
+
+    do {
+        console.log(d + "<br/>");
+        d++;
+    }
+    while (d <= 25); // this prints out numbers from 20 to 25
+    // the loop will always be executed at least  once, even if the condition is false, ..............because the code block is executed  before the condition is tested
