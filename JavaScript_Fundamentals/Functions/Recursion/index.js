c0989ab1e11d39fa49767993d89630de466703cd
Rodgers Nyangweso <rodgerso65@gmail.com>
2022-12-10 21:09:07 +0300

    ++ recursive functions

--- JavaScript_Fundamentals/Functions/Recursion/index.js
+++ JavaScript_Fundamentals/Functions/Recursion/index.js
@@ -1,3 +1,6 @@
+// Recursion
+
+// Example 1: calculate the sum of natural numbers from 1 to n using the recursion
 function sum(n) {
     if (n <= 1) {
         return n;
@@ -5,3 +8,18 @@ function sum(n) {
     return n + sum(n -1);
 }
 console.log(sum(4)) // 10
+
+// Example 2: program to count down numbers to 1
+function countDown(number) {
+    // display the number
+    console.log(number);
+
+    // decrease the number by value
+    const newNumber = number - 1;
+
+    // base case
+    if (newNumber > 0) {
+        countDown(newNumber);
+    }
+}
+console.log(countDown(4)); // 4, 3, 2, 1
