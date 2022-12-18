e6584691315c8fb8e861d1ffbfb8932c5893d5e5
Rodgers Nyangweso <rodgerso65@gmail.com>
2022-12-18 16:43:11 +0300

    ++ fizzBuzz and reverse-character functions examples

---
+++ JavaScript_Fundamentals/Functions/index.js
@@ -0,0 +1,15 @@
+// Functions in JS
+
+// Function Declaration
+functionDeclaration();
+
+function functionDeclaration() {
+    console.log("Function Declaration");
+}
+
+// Function Expression
+var functionExpression = function () {
+    console.log("Function Expression");
+};
+
+console.log(functionExpression());
