a382b71492f441dab8f08893f2a9fdbabc7cffa9
Rodgers Nyangweso <rodgerso65@gmail.com>
2022-12-19 14:53:31 +0300

    deleted Object Oriented Programming Folder

--- Object_Oriented_Programming_in_JavaScript/Array_Objects/script_1_random_number_generation.js
+++
@@ -1,7 +0,0 @@
-// Generate an arry of numbers with numbers form 0 to max
-var numbersArray = [], max = 100;
-
-for (
-    var i=1; numbersArray.push(i++) < max;
-);
-console.log(numbersArray); // Output: numbers = [1, 2, 3, -- 100]
