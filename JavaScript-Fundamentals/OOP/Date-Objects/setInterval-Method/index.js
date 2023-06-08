d8282f1e151e4e1f00374dc8e767d81804d9a9a1
Rodgers Nyangweso <rodgers.nyangweso@kyosk.app>
2023-06-08 22:05:12 +0300

    created a setInterval folder

---
+++ JavaScript-Fundamentals/OOP/Date-Objects/setInterval-Method/index.js
@@ -0,0 +1,16 @@
+// setInterval() Method
+
+// print "greetings" every 1 second
+function greet() {
+    console.log("greetings");
+}
+
+setInterval(greet, 1000); // uncomment
+
+// print "greetings" every 1 second with additional parameters
+
+function greetMe() {
+    console.log(`greetings, ${name}!`);
+}
+
+setInterval(greetMe, 1000, 'Nyangweso'); // uncomment
