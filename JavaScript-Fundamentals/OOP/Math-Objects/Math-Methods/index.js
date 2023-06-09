ccc7b426688538262efed2ea9fe20dd50fdb3f78
Rodgers Nyangweso <rodgers.nyangweso@kyosk.app>
2023-06-09 17:39:40 +0300

    folder for math methods

---
+++ JavaScript-Fundamentals/OOP/Math-Objects/Math-Methods/index.js
@@ -0,0 +1,9 @@
+
+
+// Math Methods
+
+// Number to Integer
+console.log("Round: ", Math.round(4.6)); // Output: 5
+console.log("Ceil: ", Math.ceil(4.6)); // Output: 5
+console.log("Ceil: ", Math.ceil(4.01)); // Output: 5
+console.log("Floor: ", Math.floor(4.9)); // Output: 4
