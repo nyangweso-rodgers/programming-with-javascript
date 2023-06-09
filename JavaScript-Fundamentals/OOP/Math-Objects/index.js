e54611e860aca5e419448de3ba1d9209e249861b
Rodgers Nyangweso <rodgers.nyangweso@kyosk.app>
2023-06-09 17:39:15 +0300

    deleted 2 files

--- JavaScript-Fundamentals/OOP/Math-Objects/index.js
+++
@@ -1,9 +0,0 @@
-// Math Objects in JavaScript
-
-
-// Program which asks for user input for a number and computes its square root
-var x = document.getElementById("square-root");
-var randomNumber = prompt("Enter a Random Number: ", " ");
-var squareRoot = Math.sqrt(randomNumber);
-
-x.innerHTML = "The Square Root Of " + randomNumber + " is " + squareRoot;
