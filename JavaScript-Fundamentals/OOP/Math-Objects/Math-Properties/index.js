58ce3ac5198a73bd28b9864d123039481fa237ec
Rodgers Nyangweso <rodgers.nyangweso@kyosk.app>
2023-06-09 17:24:38 +0300

    8 Math constant properties

---
+++ JavaScript-Fundamentals/OOP/Math-Objects/Math-Properties/index.js
@@ -0,0 +1,20 @@
+// E
+console.log("E: ", Math.E); // Output: 2.718281828459045
+
+// LN2
+console.log("LN2: ", Math.LN2); // Output:  0.6931471805599453
+
+// LN10
+console.log("LN10: ", Math.LN10); // Output: 2.302585092994046
+
+// LOG2E
+console.log("LOG2E: ", Math.LOG2E); // Output: 1.4426950408889634
+
+// LOG10E
+console.log("LOG10E: ", Math.LOG10E); // Output:  0.4342944819032518
+
+// Pi - returns the constant pi
+console.log("pi: ", Math.PI); // Output: 3.141592653589793
+
+// SQRT2
+console.log("sqrt2: ", Math.SQRT2); // Output: 1.4142135623730951
