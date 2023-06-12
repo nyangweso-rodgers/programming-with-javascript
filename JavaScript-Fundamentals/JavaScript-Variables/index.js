2f062ca86588702dcd13ddef811c06aadfe4dd2b
Rodgers Nyangweso <rodgers.nyangweso@kyosk.app>
2023-06-12 19:07:42 +0300

    deleted index.js file

--- JavaScript-Fundamentals/JavaScript-Variables/index.js
+++
@@ -1,8 +0,0 @@
-// Method 1: using let keyword
-let message;
-
-// now, we can put some data into it by using the assignment operator =;
-message = 'Hallo World!'; // store the string
-
-// the string is now saved into the memory area associated with the variable and we can access it using the variable name
-console.log(message); // Output: Hallo World!
