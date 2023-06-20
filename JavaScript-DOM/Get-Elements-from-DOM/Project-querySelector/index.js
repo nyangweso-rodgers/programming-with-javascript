18cfbab3e0fd84d0bdf807e325ab2567df416fb2
Rodgers Nyangweso <rodgers.nyangweso@kyosk.app>
2023-06-20 22:43:24 +0300

    ++ using querySelector to change the textContent

---
+++ JavaScript-DOM/Get-Elements-from-DOM/Project-querySelector/index.js
@@ -0,0 +1,10 @@
+
+
+// get firstStudnet using querySelector
+let firstStudent = document.querySelector('#firstStudent');
+
+console.log(firstStudent);
+console.log(firstStudent.textContent); // Output: Student 1
+
+// change the text
+firstStudent.textContent = 'Nyangweso';
