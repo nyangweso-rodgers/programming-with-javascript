5f9184be6b8e3d2cff674f1f14efca0bf3da5ac6
Rodgers Nyangweso <rodgers.nyangweso@kyosk.app>
2023-07-17 14:26:13 +0300

    ++ how to access DOM elements

--- JavaScript-DOM/Access-DOM-Elements/Project-Access-DOM-Elements/index.js
+++ JavaScript-DOM/Access-DOM-Elements/Project-Access-DOM-Elements/index.js
@@ -1,15 +1,21 @@
 // Access DOM Elements
 
 // getElementById()
-const headerElement = getElementById('headerSection');
+const headerElement = document.getElementById('headerSection');
 
 // getElementByTagName()
-const divElements = getElementByTagName('div');
+const divElements = document.getElementsByTagName('div');
 // retrieve p elements from div
-const pElementsFromDiv = divElements[0].getElementByTagName('p');
+//const pElementsFromDiv = divElements[0].getElementByTagName('p');
 
 // getElementByClassName(); // all container classes
-const container = getElementByClassName('container');
+const container = document.getElementsByClassName('container');
 
 // getElementByName(); // get email element(s)
-const emailElement = getElementByName('email');
+const emailElement = document.getElementsByName('email');
+
+// document.querySelector('input');
+// get the first project using querySelector
+const firstProject = document.querySelector('#firstProject');
+console.log(firstProject);
+console.log(firstProject.textContent);
