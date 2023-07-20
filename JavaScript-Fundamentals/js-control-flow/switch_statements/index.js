fb407336c68b6607b2091fdd2f5bf6ada7f12e6c
Rodgers Nyangweso <rodgers.nyangweso@kyosk.app>
2023-07-20 11:10:18 +0300

    added readme file for switch statement in js

--- JavaScript-Fundamentals/js-control-flow/switch_statements/index.js
+++ JavaScript-Fundamentals/js-control-flow/switch_statements/index.js
@@ -1,46 +1,5 @@
 // switch statements
 
-var day = 8;
-switch (day) {
-    case 1:
-        console.log("Monday");
-        break;
-    case 2:
-        console.log("Tuesday");
-        break;
-    case 3:
-        console.log("Wednesday");
-        break;
-    case 4:
-        console.log("Thursday");
-        break;
-    case 5:
-        console.log("Friday");
-        break;
-    case 6:
-        console.log("Saturday");
-        break;
-    case 7:
-        console.log("Sunday");
-        break;
-    default:
-        console.log("Error in the specified date!")
-}; // Error in the specified date!
-
-// example 2
-var c = 4;
-
-switch (c) {
-    case 1:
-        console.log(c + 1);
-        break;
-    case 2:
-        console.log(c + 2);
-        break;
-    default:
-        console.log(c + 1);
-}; // Output: 5
-
 // example 3
 var color = 'yellow';
 
