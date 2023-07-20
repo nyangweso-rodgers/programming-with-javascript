a09e156750894fe36b5b7a5f760eb71f8e9e5467
Rodgers Nyangweso <rodgers.nyangweso@kyosk.app>
2023-07-20 10:31:20 +0300

    ++ added a readme file for describing an if else statements

---
+++ JavaScript-Fundamentals/js-control-flow/if_statements/Readme.md
@@ -0,0 +1,35 @@
+# js if statement
+
+## Table of Contents
+
+# Task 1: Compare two numbers with an `if statement`
+```js
+    var a = 7;
+    var b = 10;
+
+    if (a < b) {
+        console.log("JS is starting to get easier");
+    };
+```
+
+# Task 2: Compare two numbers with an `if else statement`
+```js
+    // else statement
+    var a = 7;
+    var b = 10;
+
+    if (a > b) {
+        console.log("A is greater than B");
+    }
+    else {
+        console.log("A is less than B");
+    }; // A is less than B
+```
+
+# Task 3: Compare two numbers using `?` Operator
+```js
+    // using ? operator
+    var a = 7;
+    var b = 10;
+    a > b? console.log("A is greater than B"): console.log("Not Allowed!"); // Not Allowed
+```
--- JavaScript-Fundamentals/js-control-flow/if_statements/index.js
+++ JavaScript-Fundamentals/js-control-flow/if_statements/index.js
@@ -1,27 +1,5 @@
 // if statements
 
-var a = 7;
-var b = 10;
-
-if (a < b) {
-    console.log("JS is starting to get easier");
-}; // JS is starting to get easier
-
-// else statement
-var a = 7;
-var b = 10;
-
-if (a > b) {
-    console.log("A is greater than B");
-}
-else {
-    console.log("A is less than B");
-}; // A is less than B
-
-// using ? operator
-var a = 7;
-var b = 10;
-a > b? console.log("A is greater than B"): console.log("Not Allowed!"); // Not Allowed
 
 // else if statement
 var dayOfWeek = 'Monday';
@@ -36,17 +14,3 @@ else {
     console.log("Day is not specififed");
     // THE FINAL else block will be executed when none of the conditions is true
 };
-
-// Example 2: else if statement
-
-var onLineStatus = 1;
-var message;
-
-if (onLineStatus == 1) {
-    message = "User is online!";
-} else if (onLineStatus == 0) {
-    message = "User is Offline!";
-} else {
-    message = "User is away";
-};
-console.log(message);
