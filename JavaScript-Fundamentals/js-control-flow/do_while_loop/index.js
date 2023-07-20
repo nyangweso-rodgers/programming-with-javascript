3b51a5305ca8c5580448c96662f3796ec0e84b6c
Rodgers Nyangweso <rodgers.nyangweso@kyosk.app>
2023-07-20 11:15:48 +0300

    added a readme file for the do -- while loop in js

--- JavaScript-Fundamentals/js-control-flow/do_while_loop/index.js
+++ JavaScript-Fundamentals/js-control-flow/do_while_loop/index.js
@@ -1,10 +1,10 @@
 // do -- while loop
 
-var d = 20;
+var d = 1;
 
     do {
         console.log(d + "<br/>");
         d++;
     }
-    while (d <= 25); // this prints out numbers from 20 to 25
+    while (d <= 10); // this prints out numbers from 20 to 25
     // the loop will always be executed at least  once, even if the condition is false, ..............because the code block is executed  before the condition is tested
