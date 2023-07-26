d52ac539c3a93b132bf0c751c45166e35d1e693f
Rodgers Nyangweso <rodgers.nyangweso@kyosk.app>
2023-07-26 23:57:39 +0300

    edited functions folder

---
+++ JavaScript-Fundamentals/js-Functions/index.js
@@ -0,0 +1,14 @@
+// Declare Functions in js
+
+// function declarations
+
+
+// function expressions
+
+
+// Array functions
+let sumFunction = (a, b) => {
+    return a + b;
+};
+
+console.log(sumFunction(1, 2)); // Output: 3
