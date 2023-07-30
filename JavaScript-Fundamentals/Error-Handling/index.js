d1cbfea358dd48ee868412c8dbfda37b2409468f
Rodgers Nyangweso <rodgers.nyangweso@kyosk.app>
2023-07-30 14:13:30 +0300

    deleted a folder

--- JavaScript-Fundamentals/Error-Handling/index.js
+++
@@ -1,9 +0,0 @@
-// Error Handling in JS
-
-try {
-    console.log('gets executed w/o errors');
-
-    // no error here, so the catch will be skipped
-} catch (err) {
-    console.log('Catch is ignored, because there are no errors');
-}
