d2fb85261c27072bc588e3d64fbbe8929c9f27f8
Rodgers Nyangweso <rodgers.nyangweso@kyosk.app>
2023-08-16 16:05:47 +0300

    working with strings in js folder

---
+++ js-Fundamentals/js-Data-Types/js-Strings/index.js
@@ -0,0 +1,14 @@
+// string data type
+
+// let firstName = String('rodgers');
+
+// let lastName = 'Nyangweso';
+
+// let fullName = firstName.concat(lastName);
+
+let fullName = 'Rodgers Nyangweso';
+let firstName = fullName.split(' ');
+let nameLetters = fullName.split('');
+
+console.log(nameLetters); // Output: ['R', 'o', 'd', 'g', 'e', 'r', 's', ' ', 'N', 'y', 'a', 'n', 'g', 'w', 'e', 's', 'o']
+  
