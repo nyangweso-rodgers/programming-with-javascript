28a82281e3708da2573a265bffbd60a07bbcbfa6
Rodgers Nyangweso <rodgerso65@gmail.com>
2023-12-17 16:56:05 +0300

    added an app.js script

---
+++ js-Fundamentals/js-Functions/app.js
@@ -0,0 +1,27 @@
+// Functions
+
+// Function Declaration
+function divideFunction(dividend, divisor) {
+  try {
+    // Attempt the division
+    if (divisor === 0) {
+      throw new Error("Division by zero is not allowed.");
+    }
+
+    const result = dividend / divisor;
+    return result;
+  } catch (error) {
+    // handle error
+    console.error("Error: ", error.message);
+    return undefined; // or any other value to indicate failure
+  } finally {
+    // Code in this block will execute regardless of whether an error occurred or not
+    console.log("Division operation completed.");
+  }
+}
+
+console.log(divideFunction(6, 2));
+
+// Function Expression
+
+// Arrow Functions
