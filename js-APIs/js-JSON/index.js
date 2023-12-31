fb2e83545ede1d6f86447238908031709d0cc243
Rodgers Nyangweso <rodgerso65@gmail.com>
2023-12-31 12:39:20 +0300

    deleted js-JSON folder inside the APIs folder

--- js-APIs/js-JSON/index.js
+++
@@ -1,6 +0,0 @@
-// json
-
-
-const personJSON = '{"firstName" : "Rodgers", "lastName" : "Nyangweso", "gender" : "male", "age" : 20}'
-const personObject = JSON.parse(personJSON);
-console.log(personObject); //
