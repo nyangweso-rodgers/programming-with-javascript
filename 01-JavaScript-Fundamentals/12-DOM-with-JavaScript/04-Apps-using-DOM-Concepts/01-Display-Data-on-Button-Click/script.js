78eff77d72a8fcf8ccec7d147fce441cb1dcbb3f
nyangweso-rodgers <rodgerso65@gmail.com>
2024-02-07 21:26:11 +0300

    styled the web page and decoupled the 3 functions

--- 01-JavaScript-Fundamentals/12-DOM-with-JavaScript/04-Apps-using-DOM-Concepts/01-Display-Data-on-Button-Click/script.js
+++ 01-JavaScript-Fundamentals/12-DOM-with-JavaScript/04-Apps-using-DOM-Concepts/01-Display-Data-on-Button-Click/script.js
@@ -1,52 +1,45 @@
-//
+// Display Data on a Button Click
 
-// Event 1: Alert Message, when button is clicked
+// get document elements
 const btnForAlertMessage = document.getElementById("btnForAlertMessage");
-// console.log(btnForAlertMessage); // Output:
-
-const alertMessage = () => {
-  btnForAlertMessage.addEventListener("click", () => {
-    alert("This is an elrst message, button has been clicked!");
-  });
-};
-
-alertMessage();
-
-// Event 2: Display Hello World!
 const btnToDisplayText = document.getElementById("btnToDisplayText");
-
-// Function to display text when button is clicked
-const displayText = () => {
-  // Adding a click event listener to the button
-  btnToDisplayText.addEventListener("click", () => {
-    // Changing the inner HTML of the button to "Hello World!"
-    btnToDisplayText.innerHTML = "Hello World!";
-  });
-};
-
-// Calling the displayText function to set up the event listener
-displayText();
-
-// Event 3: Display Date and Time
 const btnToDisplayDateAndTime = document.getElementById(
   "btnToDisplayDateAndTime"
 );
-const displayDateAndTime = () => {
-  btnToDisplayDateAndTime.addEventListener("click", () => {
-    btnToDisplayDateAndTime.innerHTML = Date();
-  });
-};
-
-displayDateAndTime();
-
-// Event 4: Display Random Number
 const btnToDisplayRandomNumber = document.getElementById(
   "btnToDisplayRandomNumber"
 );
-const displayRandomNumber = () => {
-  btnToDisplayRandomNumber.addEventListener("click", () => {
-    btnToDisplayRandomNumber.innerHTML = Math.random();
-  });
+
+// get alert message
+const displayAlertMessage = () => {
+  let alertMessage = "This is an alert message, button has been clicked!";
+  alert(alertMessage);
+  console.log(alertMessage);
 };
 
-displayRandomNumber();
+// function 2: Display Hello World!
+const displayHelloWorld = () => {
+  let helloWorldMessage = "Hello World!";
+  btnToDisplayText.innerHTML = helloWorldMessage;
+};
+
+// function 3: Display Current Date and Time
+const displayCurrentDateAndTime = () => {
+  let currentDateAndTime = new Date();
+  btnToDisplayDateAndTime.innerHTML = currentDateAndTime;
+};
+
+// function 4: display random number
+const displayRandomNumber = () => {
+  const randomNumberCard = document.getElementById("randomNumberCard");
+  let randomNumber = Math.random();
+  const spanElement = document.createElement("span");
+  spanElement.textContent = randomNumber;
+  randomNumberCard.appendChild(spanElement);
+};
+
+// Add event handlers
+btnForAlertMessage.addEventListener("click", displayAlertMessage);
+btnToDisplayText.addEventListener("click", displayHelloWorld);
+btnToDisplayDateAndTime.addEventListener("click", displayCurrentDateAndTime);
+btnToDisplayRandomNumber.addEventListener("click", displayRandomNumber);
