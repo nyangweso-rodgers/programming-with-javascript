06a8979298f75386d214fbfb605637c053647675
nyangweso-rodgers <rodgerso65@gmail.com>
2024-02-12 22:42:44 +0300

    added string data type in js

--- 01-JavaScript-Fundamentals/01-Data-Types-in-JavaScript/01-JavaScript-Strings/index.js
+++ 01-JavaScript-Fundamentals/01-Data-Types-in-JavaScript/01-JavaScript-Strings/index.js
@@ -1,14 +1,14 @@
 // string data type
 
-// let firstName = String('rodgers');
+let firstName = String("Rodgers");
+
 
 // let lastName = 'Nyangweso';
 
 // let fullName = firstName.concat(lastName);
 
-let fullName = 'Rodgers Nyangweso';
-let firstName = fullName.split(' ');
-let nameLetters = fullName.split('');
+let fullName = "Rodgers Nyangweso";
+//let firstName = fullName.split(' ');
+let nameLetters = fullName.split("");
 
 console.log(nameLetters); // Output: ['R', 'o', 'd', 'g', 'e', 'r', 's', ' ', 'N', 'y', 'a', 'n', 'g', 'w', 'e', 's', 'o']
-  
