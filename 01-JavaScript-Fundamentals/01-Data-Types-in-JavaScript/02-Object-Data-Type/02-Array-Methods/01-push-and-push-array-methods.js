5c636447c92460cb30873335356f6340b0275e15
nyangweso-rodgers <rodgerso65@gmail.com>
2024-02-13 22:28:12 +0300

    deleted push and pop methods file

--- 01-JavaScript-Fundamentals/01-Data-Types-in-JavaScript/02-Object-Data-Type/02-Array-Methods/01-push-and-push-array-methods.js
+++
@@ -1,7 +0,0 @@
-// push and pop methods
-
-const students = ["Rodgers", "Nyangweso"];
-
-console.log(students); // Output: [ 'Rodgers', 'Nyangweso' ]
-students.push("Omondi");
-console.log(students); // Output: [ 'Rodgers', 'Nyangweso', 'Omondi' ]
