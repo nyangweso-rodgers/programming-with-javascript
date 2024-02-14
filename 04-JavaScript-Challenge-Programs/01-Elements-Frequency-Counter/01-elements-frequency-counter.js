18d75fa161da3f17c4297d501903e9e0ebcfb337
nyangweso-rodgers <rodgerso65@gmail.com>
2024-02-14 23:11:35 +0300

    option 2 to identifying non-unique elements in an array

--- 04-JavaScript-Challenge-Programs/01-Elements-Frequency-Counter/01-elements-frequency-counter.js
+++ 04-JavaScript-Challenge-Programs/01-Elements-Frequency-Counter/01-elements-frequency-counter.js
@@ -2,6 +2,7 @@
 
 const sampleArray = [1, 2, 3, 4, 5, 5];
 
+// Option 1
 const elementsFrequencyCounter = (array) => {
   const arrayCounterObject = {};
 
@@ -16,4 +17,13 @@ const elementsFrequencyCounter = (array) => {
   return arrayCounterObject;
 };
 
-elementsFrequencyCounter(sampleArray);
+// Option 2
+const NonUniqueElementsArray = (array) => {
+  let nonUniqueElements = array.filter((element, index) => {
+    return array.indexOf(element) !== index;
+  });
+  console.log("Non unique elements: ", nonUniqueElements);
+};
+
+elementsFrequencyCounter(sampleArray); // Output: Array Counter Object:  { '1': 1, '2': 1, '3': 1, '4': 2, '5': 2 }
+NonUniqueElementsArray(sampleArray); // Output: Non unique elements: [5]
