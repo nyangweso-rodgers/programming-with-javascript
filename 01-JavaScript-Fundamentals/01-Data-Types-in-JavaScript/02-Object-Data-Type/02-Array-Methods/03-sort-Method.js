8c52f87720b4a73c8dd37ee755ec83df691b43e4
nyangweso-rodgers <rodgerso65@gmail.com>
2024-02-14 11:38:14 +0300

    deleted folder

--- 01-JavaScript-Fundamentals/01-Data-Types-in-JavaScript/02-Object-Data-Type/02-Array-Methods/03-sort-Method.js
+++
@@ -1,7 +0,0 @@
-// sort() method
-const numbersArray = [2, 9, 3, 5, 1, 8];
-
-const sortNumbers = (array) => {
-  return array.sort().reverse();
-};
-console.log(sortNumbers(numbersArray)); // Output: [ 9, 8, 5, 3, 2, 1 ]
--- 01-JavaScript-Fundamentals/01-Data-Types-in-JavaScript/02-Object-Data-Type/02-Array-Methods/04-reverse-Method.js
+++
@@ -1,9 +0,0 @@
-// reverse() Method
-
-var priceList = [10, 20, 30, 40, 50];
-
-const reverseFunction = (array) => {
-  return array.reverse();
-};
-
-console.log(reverseFunction(priceList)); // Output: [ 50, 40, 30, 20, 10 ]
