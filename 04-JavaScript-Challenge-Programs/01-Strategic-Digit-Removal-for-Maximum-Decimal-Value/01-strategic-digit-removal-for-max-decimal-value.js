97b85e5eb426fdac1bfe680f8dc3d4d5bf2df2c3
nyangweso-rodgers <rodgerso65@gmail.com>
2024-02-15 13:44:24 +0300

    completed the challenge program for stragetic digit removal for maximum decimal value

--- 04-JavaScript-Challenge-Programs/01-Strategic-Digit-Removal-for-Maximum-Decimal-Value/01-strategic-digit-removal-for-max-decimal-value.js
+++ 04-JavaScript-Challenge-Programs/01-Strategic-Digit-Removal-for-Maximum-Decimal-Value/01-strategic-digit-removal-for-max-decimal-value.js
@@ -13,39 +13,20 @@ const strategicDigitRemovalForMaximumDecimalValue = (
     return "Invalid input. Both parameters should be greater than 1 and not contain a zero digit.";
   } else {
     let codeString = `${firstValue}${secondValue}`;
-    //console.log(codeString);
-    //return codeString;
 
     if (codeString.length >= 2 && codeString.length <= 100) {
       // create an integer array from codeString
       let codeIntegerArray = codeString.split("").map(Number);
 
-      // Identify the critical digit (most frequent)
-      const criticalDigit = codeIntegerArray.reduce(
-        (mostFrequentDigit, digit) => {
-          const count = codeIntegerArray.filter((d) => d === digit).length;
-          return count >
-            codeIntegerArray.filter((d) => d === mostFrequentDigit).length
-            ? digit
-            : mostFrequentDigit;
-        },
-        codeIntegerArray[0]
-      ); // Initialize with first digit
-
-      let maxNumberWithoutCriticalDigit = parseInt(codeString);
-
-      // Iterate only once, removing one instance of the critical digit
-      const criticalDigitIndex = codeIntegerArray.indexOf(criticalDigit);
-      if (criticalDigitIndex !== -1) {
-        // Ensure the critical digit exists
-        codeIntegerArray.splice(criticalDigitIndex, 1); // Remove one instance
-        maxNumberWithoutCriticalDigit = parseInt(codeIntegerArray.join(""));
-      }
+      // identify critical digits
+      let criticalDigitsArray = codeIntegerArray.filter((digit, index) => {
+        return codeIntegerArray.indexOf(digit) !== index;
+      });
 
       console.log("codeString: " + codeString);
       console.log("codeIntegerArray:", codeIntegerArray);
-      //console.log("criticalDigitsArray: ", criticalDigitsArray);
-      return maxNumberWithoutCriticalDigit.toString(); // Output as a string
+      console.log("criticalDigitsArray: ", criticalDigitsArray);
+      return criticalDigitsArray;
     } else {
       return "Invalid output. The length of codeString should be between 2 and 100 inclusive.";
     }
@@ -53,7 +34,6 @@ const strategicDigitRemovalForMaximumDecimalValue = (
 };
 
 let firstValue = 12;
-let secondValue = 314;
-let criticalDigit = console.log(
-  strategicDigitRemovalForMaximumDecimalValue(firstValue, secondValue)
-);
+let secondValue = 31;
+
+strategicDigitRemovalForMaximumDecimalValue(firstValue, secondValue);
--- 04-JavaScript-Challenge-Programs/01-Strategic-Digit-Removal-for-Maximum-Decimal-Value/02-strategic-digit-removal-for-maximum-decimal-value.js
+++ 04-JavaScript-Challenge-Programs/01-Strategic-Digit-Removal-for-Maximum-Decimal-Value/02-strategic-digit-removal-for-maximum-decimal-value.js
@@ -1,34 +1,3 @@
-# Strategic Digit Removal for Maximum Decimal Value
-
-# Challenge Description
-
-- As a codebreaker, you are deciphering a code presented as a `string`, 'codeString',denoting a considerable numerical value. Additionally, you are given a character 'criticalDigit' which is a recurring element in 'codeString'.
-- The code must be optimized by excising precisely one instance of 'criticalDigit' from 'codeString'.
-- The aim is to rearrange 'codeString' into the greatest possible numerical value in decimal form after the removal.It is assured that the 'criticalDigit' is present in 'codeString' at least once.
-- Example 1:
-
-  ```js
-  //Input: codeString = "123", criticalDigit = "3";
-  // Output: 12;
-  // Explanation: There is only one '3' in '123'. After removing '3'. the result is '12'.
-  ```
-
-- Example 2:
-  ```js
-  // Input: codeString = "1231", criticalDigit = "1"
-  // Output: 231
-  // Explanation:we can remove the first "1" to get "231" or remove the second "1" to get "123"
-  // since 231 > 123, we return "231"
-  ```
-- Constraints:
-  - `2 <=codeString.length <= 100`
-  - `codeString` consists of digits from `1` to `9`
-  - `criticalDigit` is a digit from `1` to `9`
-  - `criticalDigit` occurs at least once in `codeString`
-
-# Solution
-
-```js
 // Strategic Digit Removal for Maximum Decimal Value
 
 const strategicDigitRemovalForMaximumDecimalValue = (
@@ -60,10 +29,7 @@ const strategicDigitRemovalForMaximumDecimalValue = (
         criticalDigitOccurrenceIndices.push(i);
       }
     }
-    console.log(
-      "criticalDigitOccurrenceIndices: ",
-      criticalDigitOccurrenceIndices
-    ); // TODO: Log criticalDigitOccurrenceIndices
+    console.log("criticalDigitOccurrenceIndices: ", criticalDigitOccurrenceIndices); // TODO: Log criticalDigitOccurrenceIndices
 
     // ensure criticalDigit exists at least once
     if (criticalDigitOccurrenceIndices.length === 0) {
@@ -96,4 +62,10 @@ const strategicDigitRemovalForMaximumDecimalValue = (
     }
   }
 };
-```
+
+let codeString = "1231"; // Declare as a string
+let criticalDigit = 1;
+
+console.log(
+  strategicDigitRemovalForMaximumDecimalValue(codeString, criticalDigit)
+);
