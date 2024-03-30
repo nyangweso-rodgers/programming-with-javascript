04929e94ac03d9ac59b8640c75ac3edb4498e24c
nyangweso-rodgers <rodgerso65@gmail.com>
2024-03-30 11:25:18 +0300

    how to manually setup a react app

---
+++ 02-JavaScript-Libraries/01-React/01-Setting-Up-React-App/02-Manual-Setup/react-app-from-manual-setup/src/index.js
@@ -0,0 +1,9 @@
+// index.js
+import React from "react";
+import { render } from "react-dom";
+
+const Home = () => {
+  return <h1>Manual Setup of React Application!</h1>;
+};
+
+render(<Home />, document.getElementById("app"));
---
+++ 02-JavaScript-Libraries/01-React/01-Setting-Up-React-App/02-Manual-Setup/react-app-from-manual-setup/webpack.config.js
@@ -0,0 +1,38 @@
+import path from "path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export default {
+  entry: "./src/index.js",
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
+    compress: true,
+    port: 9000,
+  },
+};
