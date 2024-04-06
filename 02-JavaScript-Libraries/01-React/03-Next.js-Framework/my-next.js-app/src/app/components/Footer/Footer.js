a266ae2e41c69ad093c9b5ae0913e3fe708f23a0
nyangweso-rodgers <rodgerso65@gmail.com>
2024-04-06 21:51:14 +0300

    added Footer component

---
+++ 02-JavaScript-Libraries/01-React/03-Next.js-Framework/my-next.js-app/src/app/components/Footer/Footer.js
@@ -0,0 +1,17 @@
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container-fluid border border-1 border-danger  p-5">
+        <div className="row">
+          <div>
+            <p>&copy;2024 Rodgers Nyamgweso</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
