ac2f827805feb6c6ad95ec04860f2d80363af473
nyangweso-rodgers <rodgerso65@gmail.com>
2024-04-10 17:47:07 +0300

    added header component

--- 02-JavaScript-Libraries/01-React/03-Next.js-Framework/my-next.js-app/src/app/Components/App.js
+++ 02-JavaScript-Libraries/01-React/03-Next.js-Framework/my-next.js-app/src/app/Components/App.js
@@ -1,13 +1,13 @@
-//import Header from "./common/Header/Header";
+import Header from "./Common/Header/Header.js";
 //import About from "./common/About/About";
 //import Footer from "./common/Footer/Footer";
 //import TextAreaComponent from "./Forms/TextArea/TextArea";
-import FormTemplate from "./Forms/FormTemplate/FormTemplate";
+//import FormTemplate from "./Forms/FormTemplate/FormTemplate.js";
 
 const App = () => {
   return (
     <>
-      <FormTemplate />
+      <Header />
     </>
   );
 };
--- 02-JavaScript-Libraries/01-React/03-Next.js-Framework/my-next.js-app/src/app/Components/Common/Header/Header.js
+++ 02-JavaScript-Libraries/01-React/03-Next.js-Framework/my-next.js-app/src/app/Components/Common/Header/Header.js
@@ -3,12 +3,49 @@ import "./Header.css";
 const Header = () => {
   return (
     <>
-      <header>
-        <nav className="">
-          <div className="">
-            <a className="logo" href="#">
+      <header className="border border-danger p-5">
+        <nav className="navbar navbar-expand-sm fixed-top">
+          <div className="container">
+            <a className="navbar-brand" href="#">
               Rodgers Nyangweso
             </a>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarCollapseContent"
+              aria-controls="#navbarCollapseContent"
+              aria-expanded="false"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div
+              id="navbarCollapseContent"
+              className="collapse navbar-collapse"
+            >
+              <ul className="navbar-nav ms-auto">
+                <li className="nav-item">
+                  <a href="#" className="nav-link">
+                    About
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a href="#" className="nav-link">
+                    Blog
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a href="#" className="nav-link">
+                    Code Snippets
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a href="#" className="nav-link">
+                    Contact
+                  </a>
+                </li>
+              </ul>
+            </div>
           </div>
         </nav>
       </header>
