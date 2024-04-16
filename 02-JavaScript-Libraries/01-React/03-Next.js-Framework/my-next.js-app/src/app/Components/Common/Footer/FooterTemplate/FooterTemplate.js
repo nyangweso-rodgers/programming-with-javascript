c882bcdfc2381efdd2ea53ac6935a51492f18500
nyangweso-rodgers <rodgerso65@gmail.com>
2024-04-16 22:39:22 +0300

    using fontawesome and designing footer elements

--- 02-JavaScript-Libraries/01-React/03-Next.js-Framework/my-next.js-app/src/app/Components/Common/Footer/FooterTemplate/FooterTemplate.js
+++ 02-JavaScript-Libraries/01-React/03-Next.js-Framework/my-next.js-app/src/app/Components/Common/Footer/FooterTemplate/FooterTemplate.js
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import styles from "../../../../page.module.css";
 
 const FooterTemplate = () => {
   return (
@@ -11,39 +12,49 @@ const FooterTemplate = () => {
             </div>
           </div>
           <div className="row">
-            <div className="col-sm-4 border border-primary">
+            <div className={`col-sm-3 ${styles.flexRow}`}>
               <div>
-                <Link
-                  className={``}
+                <a
+                  className={`fa-brands fa-linkedin`}
+                  target="_blank"
                   href="https://www.linkedin.com/in/rodgers-nyangweso-305919b4/"
-                >
-                  LinkedIn
-                </Link>
+                ></a>
               </div>
               <div>
-                <Link
-                  className={``}
+                <a
+                  className={`fa-brands fa-github`}
+                  target="_blank"
                   href="https://github.com/nyangweso-rodgers/"
-                >
-                  LinkedIn
-                </Link>
+                ></a>
               </div>
               <div>
-                <Link
-                  className={``}
+                <a
+                  className={`fa-brands fa-hashnode`}
+                  target="_blank"
                   href="https://nyangweso-rodgers.hashnode.dev/"
-                >
-                  LinkedIn
-                </Link>
+                ></a>
               </div>
               <div>
-                <Link className={``} href="https://twitter.com/R_Nyangweso">
-                  LinkedIn
-                </Link>
+                <a
+                  className={`fa-brands fa-x-twitter`}
+                  target="_blank"
+                  href="https://twitter.com/R_Nyangweso"
+                ></a>
               </div>
             </div>
-            <div className="col-sm-4 border border-secondary"></div>
-            <div className="col-sm-4 border border-danger"></div>
+            <div className="col-sm-2 border border-secondary"></div>
+            <div className={`col-sm-7 ${styles.flexRow}`}>
+              <div>
+                <span>Terms Of Use</span>
+              </div>
+              <div>
+                <span>Privacy Policy</span>
+              </div>
+              <div>
+                <span>&copy;</span> <span>{new Date().getFullYear()}</span>{" "}
+                <span>Rodgers Nyangweso</span>
+              </div>
+            </div>
           </div>
         </div>
       </footer>
--- 02-JavaScript-Libraries/01-React/03-Next.js-Framework/my-next.js-app/src/app/page.js
+++ 02-JavaScript-Libraries/01-React/03-Next.js-Framework/my-next.js-app/src/app/page.js
@@ -1,5 +1,6 @@
 
 import "bootstrap/dist/css/bootstrap.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 //import styles from "./page.module.css";
 
 import App from "./Components/App";
