f8d66afb4b94885bf7684ecf4bac18024fe1355e
nyangweso-rodgers <rodgerso65@gmail.com>
2024-04-19 21:22:12 +0300

    added surveyHeader component

---
+++ 02-JavaScript-Libraries/01-React/03-Next.js-Framework/my-next.js-app/src/app/Components/Common/Header/SurveyFormHeader/SurveyFormHeader.js
@@ -0,0 +1,17 @@
+//SurveyFormHeader
+
+const SurveyFormHeader = () => {
+  return (
+    <>
+      <header>
+        <nav className={`navbar navbar-expand-sm`}>
+          <div className={`container`}>
+            <span className={`navbar-brand`}>App Header</span>
+          </div>
+        </nav>
+      </header>
+    </>
+  );
+};
+
+export default SurveyFormHeader;
